fb23a2f26c3b214d93dbdd66c3c28d598f203db1
godigitaldevelopment15 <godigitaldevelopment15@gmail.com>
2025-01-27 11:54:41 +0530

    Add files via upload

---
+++ routes/api/auth.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const client= require('../../config/whatsapp')
+
+const router = express.Router()
+const fs = require('fs');
+
+router.get("/getqr", async (req, res) => {
+      client.getState()
+      .then((data) => {
+        if (data) {
+          console.log('if')
+          return res.json({
+              status:"error",
+              message:"already authenticated"
+            })
+        } else{
+          fs.readFile("tempStorage/last.qr", "utf-8", (err, last_qr) => {
+            if(err){
+              console.log('there is an error')
+            }
+            if (!err && last_qr) {
+              return res.json({
+                    status:"success",
+                    qr:last_qr
+              })
+            }
+        })
+      }
+      })
+      .catch((err)=>{
+        console.log('error')
+        console.log(err)
+        return res.json({
+          error:"something went wrong"
+        })
+      });
+})
+  
+router.get('/status', async(req,res)=>{
+    console.log('status')
+    const status=await client.getState()
+    if(status!='CONNECTED'){
+        console.log('not connected')
+        return res.json({
+          status
+
+        })
+    }
+    const clientInfo= client.info
+    const {pushname, wid,platform}=clientInfo
+    console.log({clientInfo})
+    return res.json({
+      status,
+      pushname,
+      clientId:wid._serialized,
+      platform
+    })
+})
+  
+router.get('/logout', async(req,res)=> {
+    try { 
+      await client.logout();
+      return res.json({
+        status:'successfully logged out',
+      })
+    } catch(err) {
+      res.json({ 
+        status  : "error",
+        message : err 
+      });
+    }
+})
+
+module.exports= router
---
+++ routes/api/broadcast.js
@@ -0,0 +1,124 @@
+const express = require('express')
+const client= require('../../config/whatsapp')
+const upload= require('../../config/multer')
+const fs = require('fs').promises;
+const { arrangeCsvToObjArray, createBroadcast, getAllBroadcast, getAllClientBroadcast, getBroadcastContacts } = require('../../modules/broadcast/service')
+const { convertTochatId, sendMessage } = require('../../modules/whatsapp/service')
+
+const router = express.Router()
+
+
+router.post('/create',upload.single('csvFile'), async(req,res)=>{
+    try {
+      const csvFile=await fs.readFile(`tempStorage/${req.file.filename}`, "utf-8")
+      if(!csvFile){
+        console.log('there is an error')
+        return res.json({
+          error:"error",
+          message:err||"somethinhg went wrong"
+        })
+      }
+      const contacts=arrangeCsvToObjArray(csvFile)
+      const clientInfo= client.info
+      const clientId=clientInfo.wid._serialized
+      await createBroadcast({
+        clientId,
+        contacts,
+        broadcastName:req.body.broadcastName      
+      })     
+      res.json({
+      status:'successful',
+      contacts
+    })
+      
+    } catch (err) {
+      console.log(err)
+      res.json({
+        status:"error",
+        message:err||"something went wrong"
+      })
+    }
+    try {
+      console.log("now deleting")
+      await fs.unlinkSync(`tempStorage/${req.file.filename}`)
+    } catch (error) {
+      console.log(error)
+    }
+})
+router.get('/getAll', async(req,res)=>{
+    try {
+    // console.log('status')
+      const broadcasts= await getAllBroadcast()
+      return res.json({
+        status:"success",
+        broadcasts
+      })    
+    } catch (err) {
+      console.log(err)
+      return res.json({
+        status:"error",
+        message: err ||"Something went wrong"
+      })    
+    }
+  
+  })
+router.get('/currentClient/get', async(req,res)=>{
+    // console.log('status')
+    try {
+      // clientInfo.wid._serialized
+      const clientInfo= client.info
+      const broadcasts= await getAllClientBroadcast(clientInfo.wid._serialized)
+      return res.json({
+        status:"success",
+        broadcasts   
+      })    
+    } catch (err) {
+      console.log(err)
+      return res.json({
+        status:"error",
+        message: err ||"Something went wrong"
+      })
+    }
+  
+})
+router.post('/send/messagetwo',async(req,res)=> {
+  try { 
+    console.log("Body shou",req.body);
+    const message =await sendMessage(client,req.body.phone,req.body.message);
+    return res.json({
+      status:"success",
+      message
+    })
+  } catch(err) {
+    console.log("Body",req.body);
+    console.log(err)
+    return res.json({ 
+      status  : "error",
+      message : err 
+    });
+  }
+});
+router.post('/send/message', async(req,res)=>{
+    try {
+      const {broadcastName, message, clientPhone}= req.body
+      const clientId= convertTochatId(clientPhone)
+      const broadcastContacts= await getBroadcastContacts({broadcastName, clientId})
+      broadcastContacts.forEach(async(contact) => {
+        await sendMessage(client,contact, message)
+      });
+      return res.json({
+        status:"success",
+        broadcastContacts,
+        message
+      })
+    } catch (err) {
+      console.log(err)
+      return res.json({
+        status:"error",
+        message:err||"something went wrong"
+      })
+    }
+  
+})
+
+module.exports=router
